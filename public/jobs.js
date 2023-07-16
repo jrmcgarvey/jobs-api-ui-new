ce77fc4252ebf85bee99ec338a530bccde9c2882
John R. McGarvey <johnmcgarvey55@gmail.com>
2023-07-16 16:51:46 -0400

    fixed logoff and add entry

--- public/jobs.js
+++ public/jobs.js
@@ -1,17 +1,21 @@
 let jobsDiv = null;
-import { enabled, setDiv, message } from "./index.js";
+import { enabled, setDiv, message, setToken } from "./index.js";
 import { showLoginRegister } from "./loginRegister.js";
 import { showAddEdit } from "./addEdit.js";
 
 export const handleJobs = () => {
   jobsDiv = document.getElementById("jobs");
-  const logoff = document.getElementById("logoff")
-  const addJob = document.getElementById("add-job")
+  const logoff = document.getElementById("logoff");
+  const addJob = document.getElementById("add-job");
   jobsDiv.addEventListener("click", (e) => {
     if (enabled && e.target.nodeName === "BUTTON") {
       if (e.target === addJob) {
+        message.textContent = "";
         showAddEdit();
       } else if (e.target === logoff) {
+        setToken(null);
+        localStorage.removeItem("token");
+        message.textContent  = "You have been logged off.";
         showLoginRegister();
       }
     }
